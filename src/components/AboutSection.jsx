0f6244b3adfc3fd0ab8e0e1ff0051d62804511f5
Mutiyev Bektas <bektasmutiev04@mail.ru>
2023-04-13 21:43:45 +0600

    I finished 2nd section

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -4,16 +4,40 @@ function AboutSection(){
             <div className="container__2">
                 <div className="about__content">
                     <div className="about__title">
-                        <h1 className="profile">Profile</h1>
-                            <p className="profile">lol</p>
-                        <h1 className="skills">Skills</h1>
-                            <p className="skills">lol</p>
-                        <h1 className="languages">Languages</h1>
-                            <p className="languages">lol</p>
-                        <h1 className="hobbies">Hobbies</h1>
-                            <p className="hobbies">lol</p>
-                        <h1 className="courses">Courses</h1>
-                            <p className="courses">lol</p>
+                        <h1 className="about__title__h1">Profile</h1>
+                        <div className="about__border">
+                        <p className="about__title__p">Hello! I am in my second year at the university IITU.My academic specialization is information security. I have no work experience in the specialty, but I practiced and developed skills on my own. I know the basics of C++, Java, Html, Css, JavaScript. Additionally, I took courses on Linux, Coursera, Cisco, Target, Video Maker. I will be glad to do an internship or an internship and gain experience in my profession.</p>
+                        </div>
+                    </div>
+                    <div className="about__list">
+                        <ul className="skills">
+                            <p className="skills">Skills</p>
+                            <li>C++</li>
+                            <li>Java</li>
+                            <li>Html</li>
+                            <li>CSS</li>
+                            <li>Critical thinking</li>
+                            <li>Focus on results</li>
+                        </ul>
+                        <ul className="languages">
+                            <p className="languages">Languages</p>
+                            <li>Russian</li>
+                            <li>Kazakh</li>
+                            <li>English</li>
+                        </ul>
+                        <ul className="hobbies">
+                            <p className="hobbies">Hobbies</p>
+                            <li>Night walk</li>
+                            <li>Listen to music alone</li>
+                            <li>Drawing</li>
+                        </ul>
+                        <ul className="courses">
+                            <p className="courses">Courses</p>
+                            <li>Linux</li>
+                            <li>Cisco</li>
+                            <li>Russian Language</li>
+                            <li>English Language</li>
+                        </ul>
                     </div>
                 </div>
             </div>
