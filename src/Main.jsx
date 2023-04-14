ffde22519263e4198ee0b9455db2a65c92ae02bd
Mutiyev Bektas <bektasmutiev04@mail.ru>
2023-04-15 01:13:39 +0600

    поменял секцию 2 на 3. 3 страницы  готовы,остались 2

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,11 +1,15 @@
 
 import HomeSection from "./components/HomeSection";
 import AboutSection from "./components/AboutSection";
+import SkillsAbout from "./components/SkillsAbout";
+
 const Main = () => {
     return (
     <main>
         <HomeSection />
+        <SkillsAbout/>
         <AboutSection />
+        
     </main>
 );
 };
---
+++ src/components/SkillsAbout.jsx
@@ -0,0 +1,18 @@
+function SkillsAbout(){
+    return(
+        <section className="skills">
+            <div className="container__3">
+                <div className="skills__content">
+                    <div className="skills__title">
+                        <h1 className="skills__title__h1">About me</h1>
+                        <p className="skills__title__p">I was born in Atyrau city. My birthday is 7th October. As for my character, I am calm, in the circle of my friends I am active, and in the new circle I am serious. I am a 2nd year student at IITU. </p>
+                        <p className="skills__title__hover"> ☞︎</p>
+                        <p className="skills__title__hover__h">☜︎</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default SkillsAbout
